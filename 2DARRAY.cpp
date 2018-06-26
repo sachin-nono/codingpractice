d3745ce58958e6c19b19245a2889386b1b346257
sachin-nono <sachinghothwal@gmail.com>
2018-06-26 16:51:23 +0530

    bug fixes

--- 2DARRAY.cpp
+++ 2DARRAY.cpp
@@ -12,7 +12,7 @@ for(int j=0;j<c;j++)
 s=s+A[i][j];
 }
 }
-cout<<"sum of elements="<<s;
+cout<<"Sum of elements:"<<s;
 }
 void individualrows(int A[][3], int r, int c)
 {
@@ -23,7 +23,7 @@ for(int j=0; j<c;j++)
 {
 s=s+ A[i][j];
 }
-cout<<"\nsum of"<<i+1<<"row="<<s;
+cout<<"\nSum of"<<i+1<<"row:"<<s;
 }
 }
 void individualcolumn(int A[][3], int r, int c)
@@ -35,7 +35,7 @@ for(int j=0; j<c;j++)
 {
 s=s+ A[j][i];
 }
-cout<<"\nsum of"<<i+1<<"column="<<s;
+cout<<"\nSum of"<<i+1<<"column:"<<s;
 }
 }
 void leftdiagonal(int A[][3],int r, int c)
@@ -49,13 +49,13 @@ if(i==j)
 s=s+ A[i][j];
 }
 }
-cout<<"\nsum of left diagonal="<<s;
+cout<<"\nSum of left diagonal:"<<s;
 }
 void rightdiagonal(int A[][3], int r, int c)
 {
 int s=0;
 int order;
-cout<<"enter the order of matrix";
+cout<<"enter the order of matrix:";
 cin>>order;
 for(int i=0; i<r;i++)
 {
@@ -65,7 +65,7 @@ if(i+j==(order-1))
 s=s+ A[i][j];
 }
 }
-cout<<"\nsum of right diagonal="<<s;
+cout<<"\nSum of right diagonal:"<<s;
 }
 
 
@@ -74,12 +74,13 @@ void main()
 clrscr();
 int x[][3]={1,2,3,4,5,6,7,8,9};
 int m,n;
-cout<<"enter the number of rows";
+cout<<"Enter number of rows:";
 cin>>m;
-cout<<"enter the number of columns";
+cout<<"Enter number of columns:";
 cin>>n;
+cout<<endl;
 
-cout<<"enter the elements of the matrix";
+cout<<"Enter elements of the matrix:\n";
 for(int p=0;p<m;p++)
 {
 for(int q=0;q<n;q++)
@@ -90,28 +91,30 @@ cin>>x[p][q];
 int ch;
 do
 {
-cout<<"\n1.sum of all elements"
-    <<"\n2. sum of individual rows"
-    <<"\n3. sum of individual columns"
-    <<"\n4.sum of left diagonal"
-    <<"\n5.sum of right diagonal"
-    <<"\n6.Exit"
-    <<"\n Enter your choice";
+cout<<"\n1.Sum of all elements."
+    <<"\n2.Sum of individual rows."
+    <<"\n3.Sum of individual columns."
+    <<"\n4.Sum of left diagonal."
+    <<"\n5.Sum of right diagonal."
+    <<"\n6.Exit."
+    <<"\n Enter your choice:";
 cin>>ch;
 switch(ch)
 {
 case 1: selements(x,m,n);
-break;
+	break;
 case 2: individualrows(x,m,n);
-break;
+	break;
 case 3: individualcolumn(x,m,n);
-break;
+	break;
 case 4: leftdiagonal(x,m,n);
-break;
+	break;
 case 5: rightdiagonal(x,m,n);
-break;
+	break;
+case 6: cout<<"OK";
+	break;
 default:
-cout<<"invalid choice";
+	cout<<"invalid choice";
 }
 }while(ch!=6);
 getch();
