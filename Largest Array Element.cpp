a434ca0e2e243cbee4828f4e30b50c24fb577a2d
sachin-nono <40526514+sachin-nono@users.noreply.github.com>
2018-07-02 15:17:06 +0530

    Update and rename Largestarrayelement.CPP to Largest Array Element.cpp

--- Largest Array Element.cpp
+++ Largest Array Element.cpp
@@ -1,31 +1,40 @@
-/*WAP to find largest element in an array
+/*
+WAP to find largest element in an array
 */
-#include"iostream.h"
-#include"conio.h"
-#include"dos.h"
-#include"stdlib.h"
-void main()
+
+#include<iostream>
+#include<dos>
+#include<cstdlib>
+
+using namespace std;
+
+int main()
 {
-clrscr();
 int n,arr[1000],max=0;
+
 cout<<"Enter size of array n<1000 :";
 cin>>n;
+
 if(n<=0||n>1000)
 	{
 	cout<<"\nEnter in the given range!!!";
 	sleep(1);
 	exit(0);
 	}
+
 cout<<"Enter elements of array:\n";
 for(int i=0;i<n;i++)
 	cin>>arr[i];
-max=arr[0];
+
+max=arr[0];       //asssuming first element of array to be largest
+
 for(i=0;i<n;i++)
 	{
 	if(arr[i]>max)
 		max=arr[i];
 	}
-cout<<"Maximum value in array is:"<<max;
-getch();
 
+cout<<"Maximum value in array is:"<<max;
+
+return 0;
 }
