9452889b049ddc68260911932ecd271df570d8d7
sachin-nono <40526514+sachin-nono@users.noreply.github.com>
2018-07-02 15:45:13 +0530

    Update and rename BASICC~1.CPP to Basic Calculator.cpp

--- Basic Calculator.cpp
+++ Basic Calculator.cpp
@@ -1,15 +1,19 @@
-/*WAP a program to create a basic calculator
+/*
+WAP a program to create a basic calculator
 */
-#include"iostream.h"
-#include"conio.h"
-#include"stdlib.h"
-void main()
+
+#include<iostream>
+#include<stdlib>
+
+using namespace std;
+
+int main()
 {
-clrscr();
 char ch,ex;
 float x,y;
 int a,b;
-nb: cout<<"Following operators are provided:\n"
+
+nb: cout<<"Following operators are provided:\n"     //nb is goto label.
 	<<"1. +\n"
 	<<"2. -\n"
 	<<"3. *\n"
@@ -17,15 +21,15 @@ nb: cout<<"Following operators are provided:\n"
 	<<"5. %\n"
 	<<"Enter your choice:";
 cin>>ch;
+
 switch(ch)
 	{
-	case '1'://if(ch==1||ch=='+')
-			{
-			cout<<"Enter two operands:\n";
-			cin>>x>>y;
-			cout<<x<<"+"<<y<<"="<<x+y;
-			}
-		break;
+	case '1':{
+		  cout<<"Enter two operands:\n";
+		  cin>>x>>y;
+		  cout<<x<<"+"<<y<<"="<<x+y;
+		  }
+		  break;
 	case '2':{
 		 cout<<"Enter two operands:\n";
 		 cin>>x>>y;
@@ -41,33 +45,39 @@ switch(ch)
 	case '4':{
 		 cout<<"Enter two operands and second operand must not be '0' :\n";
 		 cin>>x>>y;
-		 lb: if(y==0)
+		 
+		 lb: if(y==0)    //lb is a goto label. Second no should not be zero
 			{
 			cout<<"second number should not be zero!!!\nEnter again:";
 			cin>>y;
 			goto lb;
 			}
+
 		 cout<<x<<"/"<<y<<"="<<x/y;
 		 }
 		 break;
 	case '5':{
 		 cout<<"Enter two operands and second operand must not be '0' :\n";
 		 cin>>a>>b;
-		 mb: if(b==0)
-			{
+		 mb: if(b==0)   //mb is a goto label. Second no should not be zero 
+			{ 
 			cout<<"second number should not be zero!!!\nEnter again:";
 			cin>>b;
 			goto mb;
 			}
+		
 		 cout<<a<<"%"<<b<<"="<<a%b;
 		 }
 		 break;
+	
 	default:cout<<"Enter above operators only!!!\n\n";
-		cout<<"If you want to Exit, type Y or y:";
+		cout<<"If you want to Exit, type Y or y:";     
 		cin>>ex;
-		if(ex=='Y'||ex=='y')
+		
+		if(ex=='Y'||ex=='y')         //if entered choice is 'y' or 'Y', control will be passed to label nb    
 			exit(9);
 		goto nb;
 	}
-getch();
+
+return 0;
 }
