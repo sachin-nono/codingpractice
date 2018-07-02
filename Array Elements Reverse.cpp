74be72a653fd5863a2ac638b40ed28d67252c009
sachin-nono <40526514+sachin-nono@users.noreply.github.com>
2018-07-02 15:47:58 +0530

    Update and rename ArrayReverse.CPP to Array Elements Reverse.cpp

--- Array Elements Reverse.cpp
+++ Array Elements Reverse.cpp
@@ -1,24 +1,33 @@
-/*WAP to reverse elements of an array...
+/*
+WAP to reverse elements of an array...
 */
-#include"iostream.h"
-#include"conio.h"
-#include"stdlib.h"
-#include"dos.h"
-void main()
+
+#include<iostream>
+#include<cstdlib>
+#include<dos>
+
+using namespace std;
+
+int main()
 {
-clrscr();
 void condition(int);
 void reverse(int [],int);
 int arr[100],size;
+
 cout<<"Enter size of array <99 :";
 cin>>size;
-condition(size);
+
+condition(size);         //to check if entered size is in given range or not
+
 cout<<"\nEnter array elements:\n";
 for(int i=0;i<size;i++)
 	cin>>arr[i];
-reverse(arr,size);
-getch();
+
+reverse(arr,size);       //function call
+
+return 0;;
 }
+
 void condition(int size)
 {
 if(size<=0||size>99)
@@ -28,11 +37,13 @@ if(size<=0||size>99)
 	exit(0);
 	}
 }
+
 void reverse(int arr[],int size)
 {
 int swap[100],t;
 for(int i=0,j=size-1;i<size,j>=0;i++,j--)
 	swap[j]=arr[i];
+
 cout<<"\nReversed array is:\n";
 for(i=0;i<size;i++)
 	cout<<swap[i]<<"\t";
