c09b828514ef209c7f1268945d014676708c6f2a
sachin-nono <40526514+sachin-nono@users.noreply.github.com>
2018-07-02 15:50:39 +0530

    Update ARRAY SEARCH using RECURSIVE Function.CPP

--- ARRAY SEARCH using RECURSIVE Function.CPP
+++ ARRAY SEARCH using RECURSIVE Function.CPP
@@ -3,15 +3,17 @@ Enter a number N and add N more elements to an array,
 then enter an element M
 and write a recursive function which returns the first index at which M is found in the array.
 */
-#include"iostream.h"
-#include"conio.h"
-#include"dos.h"
-#include"stdlib.h"
+
+#include<iostream>
+#include<dos>
+#include<cstdlib>
+
+using namespace std;
+
 int y=1;    //used in recursive function
 
-void main()
+int main()
 {
-clrscr();
 int rec(int [],int,int);    //searching using recursive function
 void arrele(int [],int);     //enter array elements
 void check(int);
@@ -19,19 +21,22 @@ int size,arr[101],m,x;
 
 cout<<"Enter size of array (<100) :";
 cin>>size;
-check(size);
 	
-arrele(arr,size);
+check(size);                 //to check if entered size is in given range or not
+	
+arrele(arr,size);           //function call
 	
 cout<<"\nEnter element to be searched:";
 cin>>m;
 	
 x=rec(arr,size,m);
+
 if(x!=-1)
 	cout<<"First appearence of element "<<m<<" has been noticed at position:"<<x+1;
 	else
 		cout<<"This element doesnt exist in above array.";
-getch();
+
+return 0;
 }
 
 void check(int size)
