ecc9f6660db27c6028f0d26e053e9b610873e4a3
sachin-nono <40526514+sachin-nono@users.noreply.github.com>
2018-07-02 15:31:55 +0530

    Update and rename DUPLICAT.CPP to Remove Duplicate Letters.cpp

--- Remove Duplicate Letters.cpp
+++ Remove Duplicate Letters.cpp
@@ -1,27 +1,37 @@
-#include"iostream.h"
-#include"conio.h"
-#include"stdio.h"
-void main()
+/*
+Remove Duplicate Letters. 
+for ex. , if input is- hello
+output should be- helo
+*/
+#include<iostream>
+#include<stdio>
+
+using namespace std;
+
+int main()
 {
-clrscr();
 void duplicate(char []);
 char str[26];
 
 cout<<"Enter a string:";
 gets(str);
+
 duplicate(str);
-getch();
+
+return 0;
 }
 
 void duplicate(char str[])
 {
 int i=0;
+	
 while(str[i]!='\0')
 	{
 	if(str[i]==str[i+1])
 		str[i+1]=' ';
 	i+=1;
 	}
+	
 cout<<"\nAfter removing duplicates:\n";
 puts(str);
 }
