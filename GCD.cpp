b0bdeda862fa711b4c2c4c1a2995ac708ef6f866
sachin-nono <sachinghothwal@gmail.com>
2018-07-12 14:08:19 +0530

    GCD

---
+++ GCD.cpp
@@ -0,0 +1,34 @@
+/*
+    GCD ( Greatest Common Divisor )
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2, calgcd(int, int), gcd;
+    
+    cout<<"Enter two numbers :\n";
+    cin>>num1>>num2;
+    
+    gcd=calgcd(num1,num2);
+
+    cout<<"\nGCD of "<<num1<<" and "<<num2<<" is : "<<gcd<<endl;
+    
+    return 0;
+}
+
+int calgcd(int num1, int num2)
+{
+    int x;
+    
+    for(int i=1;(i<=num1||i<=num2);i++)
+    {
+        if(num1%i==0&&num2%i==0)
+            x=i;
+    }
+    
+    return x;
+}
