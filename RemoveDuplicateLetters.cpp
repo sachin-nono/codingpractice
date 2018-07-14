86e1a44cfd5c901fe99b80da8bb2079b5d5a1498
sachin-nono <sachinghothwal@gmail.com>
2018-07-14 16:23:17 +0530

    Remove duplicate characters

--- RemoveDuplicateLetters.cpp
+++ RemoveDuplicateLetters.cpp
@@ -4,34 +4,43 @@
     for ex. , if input is - hello
                  output should be - helo
 */
+
 #include<iostream>
+#include<string.h>
 
 using namespace std;
 
 int main()
 {
-void duplicate(char []);
-char str[26];
+    void duplicate(char [], int);
+    char str[26];
 
-cout<<"Enter a string:";
-gets(str);
+    cout<<"Enter a string:";
+    gets(str);
+    
+    int len=strlen(str);
+    
+    duplicate(str,len);
+    
+    cout<<"\nAfter removing duplicates:\n";
+    puts(str);
 
-duplicate(str);
-
-return 0;
+    return 0;
 }
 
-void duplicate(char str[])
+void duplicate(char str[], int len)
 {
 int i=0;
 	
 while(str[i]!='\0')
 	{
 	if(str[i]==str[i+1])
-		str[i+1]=' ';
+    {
+        for(int j=i; j<len;j++)
+            str[j]=str[j+1];
+        len-=1;
+    }
+	
 	i+=1;
 	}
-	
-cout<<"\nAfter removing duplicates:\n";
-puts(str);
 }
