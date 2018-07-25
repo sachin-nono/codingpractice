00e9d40a5674f80d629c1abade10bb1952367216
sachin-nono <sachinghothwal@gmail.com>
2018-07-25 15:50:00 +0530

    Recursion - Is mathematical expr. balanced or not

---
+++ RecursionIsBalanced.cpp
@@ -0,0 +1,67 @@
+/*
+    Recursion - Is Balenced
+    
+    Take as input a string.
+    The string is a mathematical expression e.g. "[a+{b+(c+d)+e}+f]".
+    Write a recursive function which tests if the brackets in expression are balanced or not and 
+    returns a boolean value.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char str[40];
+    bool isBalanced(char []), x;
+    
+    cout<<"Enter a string :\n";
+    gets(str);
+    
+    x = isBalanced(str);
+    
+    cout<<endl<<x<<endl;     //boolean value get printed i.e. either 1 or 0
+    
+    return 0;
+}
+
+int i=0, a=0, b=0, c=0, d=0, e=0, f=0;
+
+bool isBalanced(char str[])
+{
+    if(str[i]!='\0')
+    {
+        if(str[i]=='[')
+            ++a;
+        
+        if(str[i]==']')
+            ++b;
+        
+        if(str[i]=='{')
+            ++c;
+        
+        if(str[i]=='}')
+            ++d;
+        
+        if(str[i]=='(')
+            ++e;
+        
+        if(str[i]==')')
+            ++f;
+        
+        
+        
+        ++i;
+        isBalanced(str);
+    }
+    
+    if(a!=b)
+        return false;
+    if(c!=d)
+        return false;
+    if(e!=f)
+        return false;
+    
+    return true;
+}
